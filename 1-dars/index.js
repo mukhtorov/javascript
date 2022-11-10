501b6e926fa0b2437343332fe0264a33a65b3b00
mukhtorov <mukhtorov_1994@mail.ru>
2022-11-10 09:53:35 +0500

    birinchi dars

---
+++ 1-dars/index.js
@@ -0,0 +1,48 @@
+// '', "", ``
+
+// let name = "Webbrain Academy";
+
+// console.log("Welcome " + name + ", IT Center");
+// console.log("Welcome \nto 'Webbrain'");
+
+// console.log(true);
+// console.log(false);
+// ==============
+
+// null
+// let a;
+// let b = null;
+
+// console.log(null === undefined);
+// console.log(null == undefined);
+
+// ==============
+// let a;
+// let b = null;
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log(a + 1);
+// console.log(b + 1);
+
+// ==============
+// symbols
+// let a = Symbol("text");
+// let b = Symbol("text");
+
+// console.log(b === a);
+// console.log(b == a);
+
+// ==============
+// Object - array, obj, function
+
+let ar = ["Muhammad", 12, "Iqboljon"]; // index
+let obj = { name: "webbrain", status: "IT Center" }; // key
+let func = () => {};
+function test() {
+  console.log(ar[0]);
+  console.log(obj.name);
+}
+
+test();
+
+console.log(typeof func);
