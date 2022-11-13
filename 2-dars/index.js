20c97284bf94d6337053d8cd015c2d7130574619
mukhtorov <mukhtorov_1994@mail.ru>
2022-11-14 00:36:49 +0500

    Type Conversion Operators, Comparision

---
+++ 2-dars/index.js
@@ -0,0 +1,129 @@
+// let b = 1;
+// let a = "2";
+
+// let str1 = "webbrain";
+// let str2 = "academy";
+
+// console.log(str1 + str2);
+// console.log(isNaN(a));
+
+// console.log(b * a);
+// console.log(+a - b);
+
+// ==========
+// let a = "10";
+// let b = "10.3";
+
+// console.log(Number(b));
+// console.log(Number.parseInt(b));
+// console.log(Number.parseFloat(b));
+// console.log(parseFloat(b));
+// console.log(parseInt(b));
+
+// console.log(Number(a));
+// console.log(Number.parseInt(a));
+// console.log(Number.parseFloat(a));
+
+// ==========
+// true = 1, '1',
+// false -> 0,
+// console.log(false == 0);
+// console.log(true == 1);
+// console.log(false == null);
+// console.log(false == undefined);
+// console.log(undefined == null);
+
+// console.log(true == "1");
+// console.log(true == "a");
+// console.log(true == "2");
+// console.log(true == "0");
+
+// ==========
+// let a = 1;
+// let b = "10";
+
+// console.log(1 + (1 * 1) / 1);
+// console.log(((1 + 1) * 1) / 1);
+
+// ========== --, ++
+// a++
+// a = a+1
+
+// let a = 11; //1
+// console.log(a--); //11
+// console.log(a); // 12
+// console.log(++a); //13
+// console.log(a); // 13
+// console.log(a++); // 13
+// console.log(a++); // 14
+// console.log(++a); // 16
+
+// ==========
+
+// Obrivations
+// let a = 50;
+
+// a = a + 10; // 50+10=60
+// a += 10; // 60+10=70
+
+// console.log(a);
+
+// ==========
+// Comparision
+// let a = 10;
+// let b = 11; //number
+// let c = "11"; // string
+
+// =, ==, ==
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(c < b);
+// console.log(c <= b);
+// console.log(c >= b);
+// console.log(c == b);
+// console.log(c === b);
+// console.log(c =< b); wrong syn
+
+// let str1 = "A";
+// let str2 = "a";
+
+// console.log(str1 > str2);
+// console.log(str1 < str2);
+// console.log("sutr1" < "str2");
+
+// let a = 1;
+// let b = 1;
+// let check = true;
+
+// console.log(a == b); // !=
+// console.log(a === b); //!==
+// console.log(a != b);
+// console.log(check);
+// console.log(!check);
+
+// ==========
+// Logical Operations
+// OR ||
+// let ceo = false;
+// let admin = true;
+
+// console.log(time === true || ceo === true);
+// console.log(false || true);
+// console.log(admin || ceo);
+
+// let time = 11;
+// console.log(admin === true || ceo === true);
+
+// AND &&
+
+// Europe
+let id = false;
+let face = true;
+
+// uzbek
+let galustik = true;
+let soqol = true;
+let pul = true;
+
+console.log(galustik && soqol && pul);
