83d611b5ed1da2ca5b4203b507fde56e026baa76
mukhtorov <mukhtorov_1994@mail.ru>
2022-11-18 06:39:28 +0500

    switch and loops

---
+++ 4-dars/index.js
@@ -0,0 +1,72 @@
+// let a=4
+
+// if(a==1){}
+// if(a==2){}
+// if(a==3){}
+// if(a==4){}
+// if(a==5){}
+// if(a==6){}
+
+// console.log(0 || 1);
+// console.log(0 ?? 1);
+
+// if -> <,>,<=,>=, ==, ===
+// switch -> ===
+// let data = "bulut";
+// switch (data) {
+//   case 1:
+//     console.log("teng");
+//     break;
+//   case "bulut":
+//     console.log("bugun bulut");
+//     break;
+//   case "qor":
+//     console.log("bugun qor");
+//     break;
+//   default:
+//     console.log("no data");
+// }
+
+// console.log("Hello Muhammad");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+// console.log("Hello Abdulloh");
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// for(boshlanish,manzil, step )
+// var i = 1;
+// var sum = 0;
+// for (i = 1; i <= 12; i++) {
+//   if (i % 3 === 0) continue;
+//   console.log(` ${i}`);
+// }
+
+// for (i = 10; i >= 1; i--) {
+//   for (j = 1; j <= 10; j++) {
+//     console.log(j);
+//   }
+//   //   console.log(i);
+// }
+
+// 2lik karra jadvali
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+
+// 3lik karra jadvali
+// 3 x 1 = 2
+// 3 x 2 = 4
+// 3 x 3 = 6
+
+// outer: for (i = 1; i <= 5; i++) {
+//   console.log(i, "i");
+// }
