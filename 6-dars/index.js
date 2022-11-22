0de8145af70c8a388598783c35f8ad7c3ee61db6
mukhtorov <mukhtorov_1994@mail.ru>
2022-11-22 10:22:26 +0500

    objects - 1

---
+++ 6-dars/index.js
@@ -0,0 +1,72 @@
+// let account = "webbrain";
+
+// console.log(account);
+
+// let newObj1 = {};
+// let newObj2 = new Object();
+
+let name = "webbrain academy";
+let major = "Frontend";
+
+const ac1 = {
+  name: "Webbrain Academy",
+  major: "Frontend",
+  founded: 2022,
+  students: 1000,
+  mentors: 5,
+};
+
+for (let i in ac1) {
+  console.log(i);
+}
+
+// const ac2 = ac1;
+
+// ac1.name = "test";
+
+// Object.assign(ac2, ac1);
+
+// console.log(ac2);
+
+// const ac2 = structuredClone(ac1);
+// ac1.name = "rteta";
+// console.log(ac2);
+
+// const ac2 = {
+//   name: "Webbrain Academy",
+//   major: "Frontend",
+// };
+// const ac3 = ac1;
+
+// ac3.founded = 2022;
+
+// console.log(ac1);
+// console.log(ac3);
+
+// account = "ladjflajsdf";
+// Object.freeze(account);
+// Object.seal(account);
+// account.major = 2022;
+
+// delete account.major;
+
+// console.log(account);
+
+// let name = "major";
+// console.log(account.name);
+// console.log(account[name]);
+// console.log(account["name"]);
+
+// let key = "major";
+
+// console.log(account.major);
+// console.log(account[key]);
+// console.log(account["major"]);
+
+// console.log(account.'full name');
+// console.log(account[0]);
+// console.log(account["0"]);
+// console.log(account["full name"]);
+
+// console.log(account.surname);
+// console.log(account["name"]);
