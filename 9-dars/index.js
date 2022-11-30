43fbdd7f379a86f0109010d9882e520d80a737d7
mukhtorov <mukhtorov_1994@mail.ru>
2022-11-30 06:55:42 +0500

    string methods

---
+++ 9-dars/index.js
@@ -0,0 +1,50 @@
+// let str1 = "1   webb  \b 'rain'    ";
+// let str2 = '2 . web "brain" ';
+// let str3 = `3webb
+// ${str1}
+// \t '"
+// rain`;
+
+// let str4 = str1 + str2;
+
+// console.log(str3);
+
+// let str1 = "webbrain";
+
+// console.log(str1[1]);
+// console.log(str1[-1]);
+// console.log(str1.at(-1));
+// console.log(str1.charAt(1));
+
+// let str2 = new String("webbrain");
+// console.log(str2 == "webbrain");
+
+// console.log(str1.localeCompare(str2)); // -1, 0 ,1
+
+// let str = "webbrain ";
+
+// console.log(`${str}`.toLocaleLowerCase('en-US'));
+// console.log(`${str}`.toLowerCase());
+
+// console.log(str.indexOf("bb"));
+// console.log(str.lastIndexOf("bb", 14));
+
+// console.log(str.startsWith("web"));
+// console.log(str.endsWith("emyy"));
+
+// console.log(str.padEnd(22, "."));
+// console.log(str.padStart(22, "."));
+
+// console.log(str.slice(0, -2));
+// console.log(str.substring(2, 0));
+// console.log(str.substr(-2, 2));
+
+// let str = "web brain acad web emy";
+
+// console.log(str.split(" ", 3));
+
+// for (char of str) {
+//   console.log(char);
+// }
+
+// console.log(str.replaceAll(/Web/gi, "WEB"));
