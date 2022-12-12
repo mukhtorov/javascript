c6a6dbe54ad889f771dc9775fa7ea00e99f46328
mukhtorov <mukhtorov_1994@mail.ru>
2022-12-12 08:42:30 +0500

    date, destructure, json

---
+++ 13-dars/index.js
@@ -0,0 +1,63 @@
+// let obj = { name: "webbrain", title: "IT Center", data: { year: 199 } };
+
+// Object.entries(obj).map(([key, value], index) => {
+//   console.log(value);
+// });
+// let name = "webbrain";
+// let {
+//   name: newName,
+//   title,
+//   data: { year },
+// } = obj;
+// console.log(data.year);
+
+// const getData = (data = {}) => {
+//   console.log(data?.name);
+// };
+
+// getData();
+
+// let ar = ["apple", "orange", "Kiwi"];
+
+// let [bir, ...res] = ar;
+
+// console.log(res);
+
+// console.log(Date.now());
+// console.log(date.getTime());
+// console.log(date.getDay());
+// console.log(date.getDate());
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+
+// const getToday = (symobl) => {
+//   let date = new Date();
+//   return `Today: ${date.getDate()}${symobl}${
+//     date.getMonth() + 1
+//   }${symobl}${date.getFullYear()}`;
+// };
+
+// console.log(getToday("-"));
+// console.log(getToday("/"));
+
+// let weeks = ["Yakshanba", "Dushanba"];
+
+// let date = new Date();
+// console.log(weeks[date.getDay()].slice(0, 4));
+
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+
+// JSON
+
+// let user = {
+//   name: "webbrain",
+//   year: 1234567,
+//   data: { fullname: "webbrain academy", founded: 1999 },
+// };
+// let json = JSON.stringify(user); // obj to json
+// let obj = JSON.parse(json); // json to obj
+
+// console.log(obj);
