d701a616a3657f961c47ae600ad246c9d7c15f9b
mukhtorov <mukhtorov_1994@mail.ru>
2022-12-28 07:24:12 +0500

    dom -1 basic

---
+++ 28-dars/index.js
@@ -0,0 +1,20 @@
+// let ul = document.getElementsByTagName("ul")[0];
+
+// console.log(ul.lastElementChild.nextElementSibling);
+// // console.log(ul.parentNode);
+// // console.log(ul.parentElement);
+// // console.log(ul.childNodes);
+
+// Selectors
+
+let by = document.getElementsByTagName("body")[0];
+let parent = document.getElementsByClassName("parent");
+let id = document.getElementById("bir");
+
+let qr = document.querySelector("#bir");
+let qrAll = document.querySelectorAll("#bir");
+// bir.textContent = "<h1>Hey</h1>";
+
+// console.log(qr.innerHTML);
+// console.log(qr.innerText);
+bir.hidden = true;
